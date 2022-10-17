b8b14e26b1eab1fa84f26133e3a083735aa4f40d
Yoonjae <yunyun3599@gmail.com>
2022-10-17 21:19:35 +0900

    controller, 테스트 개발

--- src/main/java/com/psc/sample/webflux/controller/BaseController.java
+++ src/main/java/com/psc/sample/webflux/controller/BaseController.java
@@ -3,9 +3,13 @@ package com.psc.sample.webflux.controller;
 import com.psc.sample.webflux.domain.Cart;
 import com.psc.sample.webflux.repository.CartReactiveRepository;
 import com.psc.sample.webflux.repository.ItemReactiveRepository;
+import com.psc.sample.webflux.service.CartService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
@@ -15,6 +19,7 @@ public class BaseController {
 
     private ItemReactiveRepository itemReactiveRepository;
     private CartReactiveRepository cartReactiveRepository;
+    private CartService cartService;
 
 
     @GetMapping("/")
@@ -23,5 +28,25 @@ public class BaseController {
                 .modelAttribute("items", itemReactiveRepository.findAll())
                 .modelAttribute("cart", cartReactiveRepository.findById("My Cart")
                         .defaultIfEmpty(new Cart("My Cart"))).build());
+    }
+
+    @PostMapping("/add/{id}")
+    Mono<String> addCart(@PathVariable String id) {
+        return cartService.addToCart("My Cart", id).thenReturn("redirect:/");
+    }
+
+    @DeleteMapping("/delCartCount/{id}")
+    Mono<String> delToCartCount(@PathVariable String id) {
+        return cartService.delToCartCount("My Cart", id).thenReturn("redirect:/");
+    }
+
+    @DeleteMapping("/delCartAll/{id}")
+    Mono<String> delToCartAll(@PathVariable String id) {
+        return cartService.delToCartAll("My Cart", id).thenReturn("redirect:/");
+    }
+
+    @DeleteMapping("/delItem/{id}")
+    Mono<String> deleteItem(@PathVariable String id) {
+        return itemReactiveRepository.deleteById(id).thenReturn("redirect:/");
     }
 }
